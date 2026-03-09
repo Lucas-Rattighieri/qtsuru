6fff96cab6260d52d19f9ae46eff8cb09c0d7c4d
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2026-03-09 10:29:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="qsim",
+    name="qtsuru",
     version="0.1.0",
     author="Lucas Rattighieri",
     author_email="lucas.rattighieri@gmail.com",
